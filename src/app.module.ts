b4654b78c333247282fd5cd02c0f6b5e61ddfa78
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2024-01-10 22:07:45 +0530

    JWT implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookomarkModule } from './bookomark/bookomark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookomarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, BookomarkModule, PrismaModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from "argon2";
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
 
     async signup(dto: AuthDto) {
         const { email, password } = dto;
@@ -21,9 +23,7 @@ export class AuthService {
                 },
             })
 
-            delete user.hash;
-
-            return user;
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === "P2002") {
@@ -44,12 +44,28 @@ export class AuthService {
         if (!user) {
             throw new ForbiddenException("Invalid Credentials");
         }
-        if (await argon.verify(user.hash, password)) {
-            delete user.hash;
-            return user;
-        } else {
+
+        const isValidPassword = await argon.verify(user.hash, password);
+        if (!isValidPassword) {
             throw new ForbiddenException("Invalid Password");
         }
 
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: string, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: this.config.get("JWT_SECRET")
+        });
+
+        return {
+            access_token: token,
+        };
     }
 }
